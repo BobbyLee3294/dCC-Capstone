3f80d4a473b07a169882d3e2c556bf2f5b612128
Bobby Lee <thayungbobby@gmail.com>
2023-02-04 17:51:40 -0500

    added comments app

---
+++ backend/comments/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from authentication.models import User
+# Create your models here.
+
+
+class Comment(models.Model):
+    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    text_body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.text_body, User.first_name)
