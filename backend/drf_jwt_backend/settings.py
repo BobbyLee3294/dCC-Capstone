f5a253f83e8fe09f7ba6a28ca869de25312ab265
Bobby Lee <thayungbobby@gmail.com>
2023-02-04 15:28:58 -0500

    removed django-ok-likes

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'likes',
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
