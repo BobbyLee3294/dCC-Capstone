d691b1d0a7fd2c02daf59fda1d4b14b44310564c
Bobby Lee <thayungbobby@gmail.com>
2023-02-04 19:27:07 -0500

    added parent field; added children & is_parent properties

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -10,9 +10,23 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
         return 'Comment {} by {}'.format(self.text_body, User.first_name)
+
+    # this filters all the replies of the comments
+    @property
+    def children(self):
+        return Comment.objects.filter(parent=self).reverse()
+
+    # this makes the comment either a parent comment or a reply
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
