9c3c90192b0dab54c7e43a74212149e78917180b
Bobby Lee <thayungbobby@gmail.com>
2023-02-04 19:24:41 -0500

    added bookshelf class &  book comment

--- backend/bookshelf/models.py
+++ backend/bookshelf/models.py
@@ -1,5 +1,19 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
 
-book = models.ForeignKey()
+
+class Bookshelf(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+
+
+class Book(models.Model):
+    # Question: Could this reduce the number of requests? Possible TODO
+    # api_id = models.CharField()
+    # api_link = models.URLField()
+    # book_info = models.JSONField()
+    pass
