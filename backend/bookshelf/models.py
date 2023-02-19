c4f0e4629f06f2f2fd4dc1c14045af75a739dbac
Bobby Lee <thayungbobby@gmail.com>
2023-02-18 22:36:01 -0500

    added description field

--- backend/bookshelf/models.py
+++ backend/bookshelf/models.py
@@ -6,6 +6,7 @@ from authentication.models import User
 class Bookshelf(models.Model):
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    description = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField()
     date_updated = models.DateTimeField()
