d4a79d888c3461070cfb4c61512a80dcb59a63a6
Bobby Lee <thayungbobby@gmail.com>
2023-03-10 14:41:45 -0500

    line 26: changed parameter to created_by to match field

--- backend/bookshelf/views.py
+++ backend/bookshelf/views.py
@@ -23,7 +23,7 @@ def user_bookshelves(request):
     if request.method == 'POST':
         serializer = BookshelfSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save(created_by=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
