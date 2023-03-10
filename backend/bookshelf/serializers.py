fc1d430ca9fdf825fb644090917046635fceb49d
Bobby Lee <thayungbobby@gmail.com>
2023-03-10 11:54:22 -0500

    created serializers.py

---
+++ backend/bookshelf/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Bookshelf, Book
+
+
+class BookshelfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookshelf
+        fields = ['book', 'name', 'description',
+                  'created_by', 'date_created', 'date_updated']
+        depth = 1
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['api_id', 'api_link', 'book_info']
