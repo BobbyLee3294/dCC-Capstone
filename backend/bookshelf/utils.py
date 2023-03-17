31606a910691209dee2e9dfcd1db851fd4b79508
Bobby Lee <thayungbobby@gmail.com>
2023-03-17 15:57:51 -0400

    created utils.py

---
+++ backend/bookshelf/utils.py
@@ -0,0 +1,10 @@
+import json
+
+
+class BookshelfUtils:
+    @staticmethod
+    def serialize_books(bookshelf):
+        books = list(bookshelf.book_set.all())
+        book_list = [book.book_info for book in books]
+        bookshelf.list_of_books = json.dumps(book_list)
+        bookshelf.save()
