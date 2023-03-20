beaade9a6b7c7044d3815ec8ea942ef1cdbcb959
Bobby Lee <thayungbobby@gmail.com>
2023-03-20 17:43:41 -0400

    added baseURL & created TODO

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,33 +2,35 @@ import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
-
+// TODO: Add the following pages: 'Bookshelf', 'Search', 'Book', 'Book Info'
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
   const [bookshelf, setBookshelf] = useState([]);
 
   useEffect(() => {
-    const fetchBookshelf = async () => {
+    const baseURL = "http://127.0.0.1:8000/api/bookshelves/";
+    const fetchBookshelfForUser = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/bookshelf/", {
+        let response = await axios.get(baseURL, {
           headers: {
             Authorization: "Bearer: " + token,
           },
         });
+        console.log("retriving bookshelves");
         setBookshelf(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    fetchBookshelf();
+    fetchBookshelfForUser();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      <p>Hello and welcome!!</p>
+      <p>I heard you like books...</p>
+      <p>{bookshelf.list_of_books}</p>
     </div>
   );
 };
