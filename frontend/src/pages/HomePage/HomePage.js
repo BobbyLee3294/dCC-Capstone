d265e11bd4045ee15078bfab4c2eb5c1afe4821a
Bobby Lee <thayungbobby@gmail.com>
2023-03-20 18:36:44 -0400

    added quote for testing & started link to bookshelf page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,35 +2,40 @@ import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
+import { Link } from "react-router-dom";
 // TODO: Add the following pages: 'Bookshelf', 'Search', 'Book', 'Book Info'
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
-  const [bookshelf, setBookshelf] = useState([]);
+  const [quote, setQuote] = useState();
 
   useEffect(() => {
-    const baseURL = "http://127.0.0.1:8000/api/bookshelves/";
-    const fetchBookshelfForUser = async () => {
-      try {
-        let response = await axios.get(baseURL, {
-          headers: {
-            Authorization: "Bearer: " + token,
-          },
-        });
-        console.log("retriving bookshelves");
-        setBookshelf(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchBookshelfForUser();
+    fetchQuote();
+    console.log("Fetching the user's info.");
   }, [token]);
+  async function fetchQuote() {
+    try {
+      // debugger;
+      const response = await axios.get("https:/quotes.rest/qod?language=en");
+      console.log(response.data.contents.quotes[0].quote);
+      setQuote(response.data.contents.quotes[0].quote);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      <p>I heard you like books...</p>
-      <p>{bookshelf.list_of_books}</p>
+      <div>
+        <h3>Something to think about {user.first_name}</h3>
+        <p>{quote}</p>
+      </div>
+      <div>
+        <Link to="/bookshelf">
+          <button>Bookshelves</button>
+        </Link>
+      </div>
     </div>
   );
 };
