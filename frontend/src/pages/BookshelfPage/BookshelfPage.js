46f595161b6d27363f19e8f664356474e00f26c1
Bobby Lee <thayungbobby@gmail.com>
2023-03-21 15:05:37 -0400

    added BookshelfList & changed URL for fetch

--- frontend/src/pages/BookshelfPage/BookshelfPage.js
+++ frontend/src/pages/BookshelfPage/BookshelfPage.js
@@ -4,6 +4,7 @@ import useAuth from "../../hooks/useAuth";
 
 // import components
 import BookshelfForm from "../../components/BookshelfForm/BookshelfForm";
+import BookshelfList from "../../components/BookshelfList/BookshelfList";
 
 const BookshelfPage = () => {
   const [user, token] = useAuth();
@@ -13,7 +14,7 @@ const BookshelfPage = () => {
     const fetchUserShelvesList = async () => {
       try {
         axios
-          .get("http://127.0.0.1:8000/api/bookshelves/2/", {
+          .get("http://127.0.0.1:8000/api/bookshelves/", {
             headers: {
               Authorization: "Bearer " + token,
             },
@@ -21,7 +22,7 @@ const BookshelfPage = () => {
           .then((response) => {
             console.log(
               "The response was found! Showing the bookshelves! " +
-                response.data
+                Object.values(response.data)
             );
             setBookshelves(response.data);
           });
@@ -41,10 +42,10 @@ const BookshelfPage = () => {
       </div>
       <div>
         {/* TODO: Create a component that will display all of the user's bookshelves */}
-        {/* <BookshelfList bookshelves={bookshelves} /> */}
+        <BookshelfList bookshelves={bookshelves} />
       </div>
       <div>
-        <h2>Create New</h2>
+        <h4>Create New</h4>
         <BookshelfForm />
       </div>
     </div>
