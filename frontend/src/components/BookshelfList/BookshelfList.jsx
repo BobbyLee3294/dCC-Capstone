f1151b12fd1f9ae8cc654619a2c735225f92e731
Bobby Lee <thayungbobby@gmail.com>
2023-03-21 15:04:33 -0400

    initalization of component

---
+++ frontend/src/components/BookshelfList/BookshelfList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookshelfList = () => {
+  return (
+    <div>
+      <div>
+        <h2>
+          This is where a list of all the user's bookshelves will be displayed.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default BookshelfList;
