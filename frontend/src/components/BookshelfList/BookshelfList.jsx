9d65333d3b7cdf3d0da1bde95d9218628f60ad63
Bobby Lee <thayungbobby@gmail.com>
2023-03-24 09:50:13 -0400

    started component; commented lines 13 -19 to prevent error

--- frontend/src/components/BookshelfList/BookshelfList.jsx
+++ frontend/src/components/BookshelfList/BookshelfList.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const BookshelfList = () => {
+const BookshelfList = ({ bookshelves }) => {
   return (
     <div>
       <div>
         <h2>
           This is where a list of all the user's bookshelves will be displayed.
         </h2>
+      </div>
+      <div>
+        {/* {Object.values(bookshelves).map((bookshelf, index) => (
+          <div key={index}>
+            <Link to={`/${bookshelf.name}`}>
+              <h3>{bookshelf.name}</h3>
+            </Link>
+          </div>
+        ))} */}
       </div>
     </div>
   );
