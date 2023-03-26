aabd05021f5950ff0172c4775423c051290f634a
Bobby Lee <thayungbobby@gmail.com>
2023-03-25 22:00:19 -0400

    added useAuth, bookshelf state and TODOs

--- frontend/src/components/BookshelfForm/BookshelfForm.jsx
+++ frontend/src/components/BookshelfForm/BookshelfForm.jsx
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
 import { bookKey } from "../../localKey";
 
 function BookshelfForm(props) {
-  const [name, setName] = useState("");
+  const BASE_URL = "http://127.0.0.1:8000/api/bookshelves/";
+  const [user, token] = useAuth();
+  const [bookshelfName, setBookshelfName] = useState("");
   const [description, setDescription] = useState("");
 
   function generateBookTitles() {
     const bookTitles = [];
     const APIKey = bookKey;
+    // TODO: Implent search capabilities for the query variable
     const query = props;
     axios
       .get(
@@ -31,43 +35,50 @@ function BookshelfForm(props) {
   function handleSubmit(e) {
     e.preventDefault();
     const bookshelfData = {
-      name: name,
+      name: bookshelfName,
       description: description,
-      list_of_books: generateBookTitles(),
+      created_by: user.id,
+      // list_of_books: generateBookTitles(),
     };
     axios
-      .post("/api/bookshelves", bookshelfData)
+      .post(BASE_URL, bookshelfData, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
       .then((response) => {
-        console.log(response.data);
-        // do something with the response, such as redirect to the new bookshelf page
+        console.log(` data for ${user.username}: ${response.data}`);
+        // TODO: do something with the response, such as redirect to the new bookshelf page
       })
       .catch((error) => {
-        console.log(error);
-        // handle the error
+        console.log(error.response.request.responseText);
       });
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          typeof="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Description:
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </label>
-      <br />
-      <button typeof="submit">Create Bookshelf</button>
-    </form>
+    <div>
+      <h2>Create Bookshelf</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            typeof="text"
+            value={bookshelfName}
+            onChange={(e) => setBookshelfName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+        <br />
+        <button typeof="submit">Create Bookshelf</button>
+      </form>
+    </div>
   );
 }
 
