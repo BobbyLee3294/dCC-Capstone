e8d4e267420005fd48eb92448e006591cf276d9f
Bobby Lee <thayungbobby@gmail.com>
2023-03-27 18:11:08 -0400

    initialized component

---
+++ frontend/src/components/Book/Book.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Book = () => {
+  return (
+    <div>
+      <h4>
+        {/* Question: Would it be better to use Google Books embedded viewer API here? */}
+        This is a book. It should display the title, the author , other metadata
+      </h4>
+    </div>
+  );
+};
+
+export default Book;
