a59f96cd458255b15b3a24c364c5828a345a62f1
Bobby Lee <thayungbobby@gmail.com>
2023-03-29 18:15:24 -0400

    removed generateBookTitles function

--- frontend/src/pages/BookshelfPage/BookshelfPage.js
+++ frontend/src/pages/BookshelfPage/BookshelfPage.js
@@ -1,47 +1,17 @@
-import { axios } from "axios";
-import React, { useCallback, useEffect } from "react";
-import { bookKey } from "../../localKey";
+import React from "react";
 
 const BookshelfPage = () => {
-  const generateBookTitles = useCallback(() => {
-    const bookTitles = [];
-    const APIKey = bookKey;
-    // TODO: Create search capabilities for the query variable
-    const query = "catcher";
-    debugger;
-    axios
-      .get(
-        `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${APIKey}`
-      )
-      .then((response) => {
-        const bookData = response.data.items;
-        bookData.forEach((book) => {
-          const title = book.volumeInfo.title;
-          bookTitles.push(title);
-        });
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-    const books = bookTitles.map((title) => ({ title }));
-    return JSON.stringify(books);
-  }, []);
-
-  useEffect(() => {
-    generateBookTitles();
-  }, [generateBookTitles]);
-
   return (
     <div>
       <div>
-        <h3>
+        <h1>
           This is a more detailed page for the bookshelf you selected from
           BookshelfList. Here it will the show the name, the description, when
           it was created, when it was last updated, and of course the list of
           books.
-        </h3>
+        </h1>
         <div>
-          <h4>This will show the name of the bookshelf.</h4>
+          <h2>This will show the name of the bookshelf.</h2>
         </div>
         <div>
           <p>
