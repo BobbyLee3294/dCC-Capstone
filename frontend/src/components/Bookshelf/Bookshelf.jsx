f5490736d3701e6d5bce3c6238065837e57e6fc2
Bobby Lee <thayungbobby@gmail.com>
2023-03-29 16:48:08 -0400

    moved comment to BookshelfPage

--- frontend/src/components/Bookshelf/Bookshelf.jsx
+++ frontend/src/components/Bookshelf/Bookshelf.jsx
@@ -1,40 +1,19 @@
 import React from "react";
+import Book from "../Book/Book";
 
-const Bookshelf = (props) => {
-  // TODO: Move this somewhere else, somewhere that needs to display book titles like Search Page or a Book component
-  // const generateBookTitles = useCallback(() => {
-  //   const bookTitles = [];
-  //   const APIKey = bookKey;
-  //   // TODO: Implent search capabilities for the query variable
-  //   const query = props;
-  //   axios
-  //     .get(
-  //       `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${APIKey}`
-  //     )
-  //     .then((response) => {
-  //       const bookData = response.data.items;
-  //       bookData.forEach((book) => {
-  //         const title = book.volumeInfo.title;
-  //         bookTitles.push(title);
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.message);
-  //     });
-  //   const books = bookTitles.map((title) => ({ title }));
-  //   return JSON.stringify(books);
-  // }, [props]);
-
-  // useEffect(() => {
-  //   generateBookTitles();
-  // }, [generateBookTitles]);
-
+const Bookshelf = ({ bookshelf }) => {
   return (
     <div>
       <div>
-        <h3>
-          This is a bookshelf where you seen the books that are placed here.
-        </h3>
+        <h4>
+          This is a bookshelf. All of the books you placed in this bookshelf
+          will be displayed here.
+        </h4>
+        <ul>
+          <ol>
+            <Book />
+          </ol>
+        </ul>
       </div>
     </div>
   );
