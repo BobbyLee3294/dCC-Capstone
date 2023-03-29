254ab5d73f9132bf14e817f68ef2d3155d1ffc18
Bobby Lee <thayungbobby@gmail.com>
2023-03-29 18:19:18 -0400

    Bookshelf component inserted

--- frontend/src/pages/BookshelfPage/BookshelfPage.js
+++ frontend/src/pages/BookshelfPage/BookshelfPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Bookshelf from "../../components/Bookshelf/Bookshelf";
 
 const BookshelfPage = () => {
   return (
@@ -11,17 +12,7 @@ const BookshelfPage = () => {
           books.
         </h1>
         <div>
-          <h2>This will show the name of the bookshelf.</h2>
-        </div>
-        <div>
-          <p>
-            This will show the description of the bookshelf. This will also show
-            the date the bookshelf was created and when it was last updated.
-          </p>
-        </div>
-        <div>
-          {/* TODO: Create a BookList component that will show a list of books in this bookshelf */}
-          <p>This is where the list of books will be displayed.</p>
+          <Bookshelf />
         </div>
       </div>
     </div>
