75bcc321d983083210fbbb0e7f549052434bb3f3
Bobby Lee <thayungbobby@gmail.com>
2023-04-04 15:56:46 -0400

    minor formatting

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Book = () => {
   return (
     <div>
-      <h5>
+      <h6>
         {/* Question: Would it be better to use Google Books embedded viewer API here? */}
         This is a book. It should display the title, the author , and other
         metadata
-      </h5>
+      </h6>
     </div>
   );
 };
