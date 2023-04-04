51971bf8ed44db7a77b3fcbdf737c48a019e9901
Bobby Lee <thayungbobby@gmail.com>
2023-04-04 17:15:18 -0400

    created generatedBookTitles

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { bookKey } from "../../localKey";
 
-const Book = () => {
+const Book = ({ book_id }) => {
+  const [bookId, setBookId] = useState("");
+  const [book, setBook] = useState([]);
+
+  const generateBookTitles = useCallback(async () => {
+    try {
+      const APIKey = bookKey;
+      const BASE_URL = `https://www.googleapis.com/books/v1/volumes/${book_id}?key=${APIKey}`;
+      axios.get(BASE_URL).then((response) => {
+        console.log("Book: " + response.data);
+        setBook(response.data);
+        setBookId(response.data.id);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [book_id]);
+
+  useEffect(() => {
+    generateBookTitles();
+  }, [generateBookTitles]);
   return (
     <div>
       <h6>
@@ -8,6 +30,7 @@ const Book = () => {
         This is a book. It should display the title, the author , and other
         metadata
       </h6>
+      <div>{bookId}</div>
     </div>
   );
 };
