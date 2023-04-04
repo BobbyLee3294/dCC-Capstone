67be808bb2df85dd39bf3acf22dcf628d616a72a
Bobby Lee <thayungbobby@gmail.com>
2023-04-04 13:22:05 -0400

    added useNavigate

--- frontend/src/components/BookshelfList/BookshelfList.jsx
+++ frontend/src/components/BookshelfList/BookshelfList.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const BookshelfList = (props) => {
+  const naviagate = useNavigate();
+  const bookshelf = { bookshelf: Object.values(props) };
+
   return (
     <div>
       <div>
         {props?.bookshelves?.map(function (el, index) {
+          const handleOnClick = () => {
+            naviagate(`/bookshelf_details/${el.name}`, {
+              state: { bookshelf },
+            });
+          };
           return (
             <div key={index}>
-              <Link to={`bookshelf/${el.name}`}>
+              {/* <Link to={`/bookshelf_details/${el.name}`}>
                 <h3>{el.name}</h3>
-              </Link>
+              </Link> */}
+              <div onClick={() => handleOnClick(el)}>{el.name}</div>
             </div>
           );
         })}
