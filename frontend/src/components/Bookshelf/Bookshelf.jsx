c05e69770c467121786fa7730105e454e43bd5a4
Bobby Lee <thayungbobby@gmail.com>
2023-04-04 15:10:51 -0400

    restructuring of component

--- frontend/src/components/Bookshelf/Bookshelf.jsx
+++ frontend/src/components/Bookshelf/Bookshelf.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import Book from "../Book/Book";
 
 const Bookshelf = ({ bookshelf }) => {
+  console.log("Bookshelf:", bookshelf);
   return (
     <div>
       <div>
-        <h4>
-          This is a bookshelf. All of the books you placed in this bookshelf
-          will be displayed here.
-        </h4>
-        <ul>
-          <ol>
-            <Book />
-          </ol>
-        </ul>
+        <h4>{bookshelf.el.name}</h4>
+        <div>
+          <span>
+            <label>Created On: </label>
+            {bookshelf.el.date_created}
+          </span>
+          <br />
+          <span>
+            <label>Last Updated On:</label>
+            {bookshelf.el.date_updated}
+          </span>
+          <br />
+          <span>
+            <label>Description: </label>
+            {bookshelf.el.description}
+          </span>
+        </div>
+        <div>
+          <label>List of Books</label>
+          <Book />
+        </div>
       </div>
     </div>
   );
