ce4ce31e06fd45d2701292071a7273b2f9300401
Bobby Lee <thayungbobby@gmail.com>
2023-04-17 08:51:13 -0400

    created SearchPage

---
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useCallback, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { bookKey } from "../../localKey";
+
+// import components
+// TODO: create the following components
+// import SearchBar from "../../components/SearchBar/SearchBar"
+import BookViewer from "../../components/BookViewer/BookViewer";
+
+const SearchPage = () => {
+  const [queryParam, setQueryParam] = useState("catcher");
+  const [savedData, setSavedData] = useState([]);
+  const { google } = require("@googleapis/books");
+  const books = google.books({
+    version: "v1",
+    auth: { bookKey },
+  });
+  const params = { queryParam };
+  async function getBooks() {
+    const response = await axios.get(params);
+    console.log(`The book URL is ${response.data.url}`);
+  }
+
+  return (
+    <div>
+      <h1>This is the search page. Here you can search and view books.</h1>
+    </div>
+  );
+};
+
+export default SearchPage;
