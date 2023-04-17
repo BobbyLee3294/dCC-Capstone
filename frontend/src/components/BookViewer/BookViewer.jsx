1841d0158238e07993befb12fa21a88b568ab547
Bobby Lee <thayungbobby@gmail.com>
2023-04-17 11:36:17 -0400

    created map for passed savedData

--- frontend/src/components/BookViewer/BookViewer.jsx
+++ frontend/src/components/BookViewer/BookViewer.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-const BookViewer = () => {
+const BookViewer = ({ savedData }) => {
+  console.log("savedData:", savedData);
+  // const authorArrToString = savedData.el.volumeInfo.authors.toString();
+  // const splitAuthorString = authorArrToString.split(",");
+
+  // console.log(splitAuthorString);
   return (
     <div>
       <h2>
-        This is a more detailed view of the book which will include other
-        metadata such as the ISBN
+        This will show the books which you can click on to see in more detail.
       </h2>
+      {savedData.map((el, index) => (
+        <div key={index}>
+          <div className="img-viewer">
+            <img
+              src={el.volumeInfo.imageLinks.thumbnail}
+              alt={el.volumeInfo.title}
+            />
+          </div>
+          <div className="bookInfo">
+            <h3>Title: {el.volumeInfo.title}</h3>
+            {/* TODO: Have authors shown like this: "Author(s): authors[0], authors[1], etc." */}
+            <h4>Author(s): {el.volumeInfo.authors}</h4>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
