fec72ecfc2b828db63031822de880d59553899fd
Bobby Lee <thayungbobby@gmail.com>
2023-04-17 11:39:57 -0400

    created function and passed props to children

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,20 +1,45 @@
 import axios from "axios";
-import React, { useCallback, useState } from "react";
-import useAuth from "../../hooks/useAuth";
+import React, { useCallback, useEffect, useState } from "react";
 import { bookKey } from "../../localKey";
 
 // import components
-// TODO: create the following components
-// import SearchBar from "../../components/SearchBar/SearchBar"
 import BookViewer from "../../components/BookViewer/BookViewer";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const SearchPage = () => {
-  const [queryParam, setQueryParam] = useState("catcher");
+  const [query, setQuery] = useState("catcher");
   const [savedData, setSavedData] = useState([]);
+  const APIKey = bookKey;
+
+  const fetchBooks = useCallback(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${APIKey}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setSavedData(response.data.items);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, [APIKey, query]);
+
+  useEffect(() => {
+    fetchBooks();
+    console.log("Collecting results for Google Books.");
+  }, [fetchBooks]);
 
   return (
     <div>
       <h1>This is the search page. Here you can search and view books.</h1>
+      <div>
+        {/* Use the following line after creation of SearchBar component */}
+        <SearchBar setQuery={setQuery} fetchBooks={fetchBooks} />
+      </div>
+      <div>
+        <BookViewer savedData={savedData} />
+      </div>
     </div>
   );
 };
