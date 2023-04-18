f4f4fe5e5000a051654f32351606f97ca4916f48
Bobby Lee <thayungbobby@gmail.com>
2023-04-18 05:46:19 -0400

    initialized component

---
+++ frontend/src/components/Book/Book.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Book = () => {
+  return (
+    <div>
+      <div>
+        <h3>This is a Book. It will show the following details:</h3>
+        <ul>
+          <ol>basic attributes</ol>
+          <ol>title</ol>
+          <ol>author</ol>
+          <ol>pageCount</ol>
+          <ol>genre</ol>
+          {/* TODO: #5 make false advanced attributes hidden (Bonus pts if we can give the user to ability to toggle this.) */}
+          <ol>advanced attributes</ol>
+          <ol>publisher</ol>
+          <ol>publicDomainAccess</ol>
+          <ol>sharableQuotes</ol>
+          <ol>sharableQuotes</ol>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
