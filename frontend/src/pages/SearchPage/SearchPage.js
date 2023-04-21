18baa9f887710c64a06b771382b85b2f1c36135a
Bobby Lee <thayungbobby@gmail.com>
2023-04-21 16:02:26 -0400

    line 10 - replaced "" with "sample"

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -7,7 +7,7 @@ import BookViewer from "../../components/BookViewer/BookViewer";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
 const SearchPage = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("sample");
   const [savedData, setSavedData] = useState([]);
   const APIKey = bookKey;
 
