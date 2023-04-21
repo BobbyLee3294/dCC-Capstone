8f798c30c44783496824eedf746317c1bb3ed2bf
Bobby Lee <thayungbobby@gmail.com>
2023-04-21 15:43:38 -0400

    cont of component layout part 2

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -4,34 +4,41 @@ const Book = ({ book }) => {
   return (
     <div>
       <div>
-        {/* <h3>This is a Book. It will show the following details:</h3>
+        {/* This is a Book. It will show the following details:
               basic attributes such as:
-              title
-              author
-              <li>pageCount</li>
-              <li>genre</li>
-              <li>description</li>
+                title
+                author
+                pageCount
+                genre
+                description
         */}
         {/* TODO: #5 make false advanced attributes hidden (Bonus pts if we can give the user to ability to toggle this.) */}
-        {/* and advanced attributes like:
-              ISBN
-              publisher
-              publicDomainAccess
-              sharableQuotes
+        {/*   and advanced attributes like:
+                ISBN
+                publisher
+                publicDomainAccess
+                sharableQuotes
          */}
         {/* TODO: #7 map out details */}
-        <div>
+        <div className="titleHeader">
           <h2>{book.volumeInfo?.title}</h2>
           <span>
             <h4>By {book.volumeInfo?.authors}</h4>
           </span>
         </div>
-        <div>
+        <div className="thumbnailImg">
+          <img
+            src={book.volumeInfo?.imageLinks.thumbnail}
+            alt={book.volumeInfo?.title}
+          />
+        </div>
+        <div className="attributesBody">
           <p>ISBN: </p>
           <p>Page Count: {book.volumeInfo?.pageCount}</p>
           <p>Published: {book.volumeInfo?.publishedDate}</p>
           <p>Publisher: {book.volumeInfo?.publisher}</p>
           <p>Genre: {book.volumeInfo?.categories}</p>
+          {/* Question: Is there another way to display the description other than this? */}
           <p
             dangerouslySetInnerHTML={{ __html: book.volumeInfo?.description }}
           />
