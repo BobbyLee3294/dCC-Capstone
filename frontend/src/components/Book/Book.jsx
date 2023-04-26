5d660c2555c5880ba6dcbc433f03c4fba9c5d413
Bobby Lee <thayungbobby@gmail.com>
2023-04-25 23:03:20 -0400

    removed uncessary comments

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -4,26 +4,10 @@ const Book = ({ book }) => {
   return (
     <div>
       <div>
-        {/* This is a Book. It will show the following details:
-              basic attributes such as:
-                title
-                author
-                pageCount
-                genre
-                description
-        */}
-        {/* TODO: #5 make false advanced attributes hidden (Bonus pts if we can give the user to ability to toggle this.) */}
-        {/*   and advanced attributes like:
-                ISBN
-                publisher
-                publicDomainAccess
-                sharableQuotes
-         */}
-        {/* TODO: #7 map out details */}
         <div className="titleHeader">
           <h2>{book.volumeInfo?.title}</h2>
           <span>
-            {/* TODO: Have authors shown like this: "Author(s): authors[0], authors[1]...etc." */}
+            {/* TODO: #13 Have authors shown like this: "Author(s): authors[0], authors[1]...etc." */}
             <h4>By {book.volumeInfo?.authors}</h4>
           </span>
         </div>
