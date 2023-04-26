f871db7502c45125116d05096a685608cae43b03
Bobby Lee <thayungbobby@gmail.com>
2023-04-25 23:04:24 -0400

    created filterByOption function

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
 
-const SearchBar = (props) => {
+const SearchBar = ({ setQuery, fetchBooks, savedData }) => {
+  console.log("savedData:", savedData);
   const [search, setSearch] = useState("");
+  const [filterOption, setFilterOption] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
-    props.setQuery(search);
-    props.fetchBooks();
+    filterByOption();
+    setQuery(search);
+    fetchBooks();
+  }
+  function filterByOption(books) {
+    if (!filterOption) {
+      return books;
+    }
+    const filterText = filterOption.toLowerCase();
+    debugger;
+    return books.filter((book) => {
+      switch (filterText) {
+        case "title":
+          return book.title.toLowerCase().includes(search.toLowerCase());
+        case "author":
+          return book.authors
+            .split()
+            .toLowerCase()
+            .includes(search.toLowerCase());
+        case "genre":
+          return book.categories
+            .split()
+            .toLowerCase()
+            .includes(search.toLowerCase());
+        default:
+          return true;
+      }
+    });
   }
   return (
     <div>
@@ -17,6 +45,15 @@ const SearchBar = (props) => {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
+          <select
+            value={filterOption}
+            onChange={(e) => setFilterOption(e.target.value)}
+          >
+            <option value=""></option>
+            <option value="title">Title</option>
+            <option value="author">Author</option>
+            <option value="Genre">Genre</option>
+          </select>
           <button id="search-btn" type="submit">
             🔍
           </button>
