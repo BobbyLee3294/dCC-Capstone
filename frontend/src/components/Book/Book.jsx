974172ac308f524619f4bc14b17ee8d05beeac36
Bobby Lee <thayungbobby@gmail.com>
2023-05-04 22:43:23 -0400

    minor reformat

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -6,9 +6,11 @@ const Book = ({ book }) => {
       <div>
         <div className="titleHeader">
           <h2>{book.volumeInfo?.title}</h2>
+          <br />
+          <h3>{book.volumeInfo?.subtitle}</h3>
           <span>
             {/* TODO: #13 Have authors shown like this: "Author(s): authors[0], authors[1]...etc." */}
-            <h4>By {book.volumeInfo?.authors}</h4>
+            <h4>By {book.volumeInfo?.authors.toString()}</h4>
           </span>
         </div>
         <div className="thumbnailImg">
@@ -19,12 +21,17 @@ const Book = ({ book }) => {
         </div>
         <div className="attributesBody">
           {/* TODO: #11 Have ISBN shown like this: "ISBN: industryIdentifiers[0], industryIdentifiers[1]" */}
-          <p>ISBN: </p>
+          <dl>
+            <dt>ISBN 10</dt>
+            <dd>- {book.volumeInfo?.industryIdentifiers[0].identifier}</dd>
+            <dt>ISBN 13</dt>
+            <dd>- {book.volumeInfo?.industryIdentifiers[1].identifier}</dd>
+          </dl>
           <p>Page Count: {book.volumeInfo?.pageCount}</p>
           <p>Published: {book.volumeInfo?.publishedDate}</p>
           <p>Publisher: {book.volumeInfo?.publisher}</p>
           {/* TODO: #12 Have genre shown like this: "Genre: categories[0], categories[1]...etc." */}
-          <p>Genre: {book.volumeInfo?.categories}</p>
+          <p>Genre: {book.volumeInfo?.categories.toString()}</p>
           {/* Question: Is there another way to display the description other than this? */}
           <p
             dangerouslySetInnerHTML={{ __html: book.volumeInfo?.description }}
