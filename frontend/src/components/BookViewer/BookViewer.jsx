934b03381a2b604415937248394fb1db7b2def37
Bobby Lee <thayungbobby@gmail.com>
2023-05-04 22:48:03 -0400

    created hasSubtitles, splitAuthors, splitGenres

--- frontend/src/components/BookViewer/BookViewer.jsx
+++ frontend/src/components/BookViewer/BookViewer.jsx
@@ -17,6 +17,52 @@ const BookViewer = ({ savedData }) => {
             state: { bookId },
           });
         };
+        const hasSubtitle = () => {
+          if (el.volumeInfo?.subtitle) {
+            return (
+              <div>
+                <h3>Title: {el.volumeInfo?.title}</h3>
+                <h3>Subtitle: {el.volumeInfo?.subtitle}</h3>
+              </div>
+            );
+          } else {
+            return (
+              <div>
+                <h3>Title: {el.volumeInfo?.title}</h3>
+              </div>
+            );
+          }
+        };
+        const splitAuthors = () => {
+          const authorArray = el.volumeInfo?.authors;
+          if (authorArray) {
+            if (authorArray.length > 1) {
+              return (
+                <div>
+                  <h3>Authors: {authorArray.toString()}</h3>
+                </div>
+              );
+            } else {
+              return <div>Author: {authorArray}</div>;
+            }
+          } else {
+            return <div>No Author</div>;
+          }
+        };
+        const splitGenres = () => {
+          const genreArray = el.volumeInfo?.categories;
+          if (genreArray) {
+            if (genreArray.length > 1) {
+              /** split this array and turn it into a string to be returned */
+              console.log(genreArray.toString());
+              return <div>Genres: {genreArray.toString()}</div>;
+            } else {
+              return <div>Genre: {genreArray}</div>;
+            }
+          } else {
+            return <div>Genre not found</div>;
+          }
+        };
         return (
           <div key={index}>
             <div className="img-viewer" onClick={handleOnClick}>
@@ -26,9 +72,10 @@ const BookViewer = ({ savedData }) => {
               />
             </div>
             <div className="bookInfo">
-              <h3>Title: {el.volumeInfo?.title}</h3>
+              {hasSubtitle()}
               {/* TODO: #4 Have authors shown like this: "Author(s): authors[0], authors[1], etc." */}
-              <h4>Author(s): {el.volumeInfo?.authors}</h4>
+              <h4>{splitAuthors()}</h4>
+              <h4>{splitGenres()}</h4>
             </div>
           </div>
         );
